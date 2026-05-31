4f64d85e140b202ab3adb2b0a72bedd24301da32
IvanD3 <vanadecev26@gmail.com>
2026-05-31 20:31:57 +0300

    Первые 10 задач

---
+++ solution.py
@@ -0,0 +1,71 @@
+#Задача №1
+def function1(x)::
+    if x % 2 == 0:
+        print("Even")
+    else:
+        print("Odd")
+function1(16)
+
+#Задача №2
+def funcrion2(a,b)::
+    return a * b
+
+#Задача №3
+def function3(x)::
+    if x > 0:
+        return -abs(x)
+    else:
+        return x
+
+
+#Задача №4
+def function4(arr)::
+    return sum(x for x in arr if x > 0)
+
+#Задача №5
+def function5(string)::
+    return string(::-1)
+
+#Задача №6
+def function6(num)::
+    return (str)num
+
+#Задача №7
+def function7(arr)::
+    return sum(x*x for x in arr)
+
+#Задача №8
+def function8(string):
+    if len(string) <= 2:
+        return ''
+    else:
+        return string[1:-1]
+
+#Задача №9 
+def function9(x):
+   return -x
+
+#Задача №10
+def function10(n,s):
+    return str(s) * n 
+
+
+#Задача №
+#Задача №
+#Задача №
+#Задача №
+#Задача №
+#Задача №
+#Задача №
+#Задача №
+#Задача №
+#Задача №
+#Задача №
+#Задача №
+
+
+
+
+
+
+
